fd8a3201a1c591ae1e42d9102e13e5cc6e2a7d91
Marko <markocrush96@gmail.com>
2018-01-04 12:22:35 +0000

    add function swap2 to functionExp

--- ed.playground/Pages/functionExp.xcplaygroundpage/Contents.swift
+++ ed.playground/Pages/functionExp.xcplaygroundpage/Contents.swift
@@ -9,3 +9,17 @@ func changeANum(currentNum: inout Int, newNum: Int) {
 changeANum(currentNum: &someNum, newNum: 15)
 
 someNum
+
+
+
+func swap2(_ a: inout Int, _ b: inout Int) {
+    let temp = a
+    a = b
+    b = temp
+}
+
+var (a, b) = (1, 2)
+
+swap2(&a, &b)
+
+print(a, b) // → 2 1
